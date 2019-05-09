8d86634323df7926b68518076e5f157870a253fe
ninya12 <ninya12@naver.com>
2019-05-09 23:17:02 +0900

    add getintersection

--- Self-Driving/getIntersection.py
+++ Self-Driving/getIntersection.py
@@ -1,52 +1,53 @@
 class Point(object):
-	"""docstring for Point"""
-	def __init__(self, x=0, y=0):
-		super(Point, self).__init__()
-		self.x = float(x)
-		self.y = float(y)
+    """docstring for Point"""
+    def __init__(self, x=0, y=0):
+        super(Point, self).__init__()
+        self.x = float(x)
+        self.y = float(y)
 
 
 def getIntersection(x1, x2, x3, x4):
-	# 두 점을 지나는 직선 끼리의 교점을 찾는 함수.
-	# 평행 여부 확인
-	parallel1 = (x1.x == x2.x)
-	parallel2 = (x3.x == x4.x)
-	# result init
-	result = Point()
-	
-	# 직선 1이 y축에 평행한 경우
-	if(parallel1):
-		sameValue1 = x1.x
-	else:
-		increase1 = (x2.y - x1.y)/(x2.x - x1.x) # 직선 1의 기울기
-		constant1 = x1.y - increase1 * x1.x # 직선 1의 y절편
-	
-	# 직선 2가 y축에 평행한 경우
-	if(parallel2):
-		sameValue2 = x3.x
-	else:
-		increase2 = (x4.y - x3.y)/(x4.x - x3.x) # 직선 2의 기울기
-		constant2 = x3.y - increase2 * x3.x # 직선 2의 y절편
+    # 두 점을 지나는 직선 끼리의 교점을 찾는 함수.
+    # 평행 여부 확인
+    parallel1 = (x1.x == x2.x)
+    parallel2 = (x3.x == x4.x)
+    # result init
+    result = Point()
 
-	# 두 직선이 평행한 경우 교점 없음. return None
-	if(parallel1 and parallel2):
-		return None
-	
-	# 직선 1이 y축에 평행한 경우 직선 1의 직선 위에 교점의 x값이 존재.
-	# 직선 2의 y = ax + b 함수에 x값을 넣어 y값을 구함.
-	if(parallel1):
-		result = Point(sameValue1, increase2 * sameValue1 + constant2)
-	# 직선 2가 y축에 평행한 경우 직선 2의 직선 위에 교점의 x값이 존재.
-	# 직선 1의 y = ax + b 함수에 x값을 넣어 y값을 구함.
-	elif(parallel2):
-		result = Point(sameValue2, increase1 * sameValue2 + constant1)
-	# y축에 평행한 직선이 없는 경우
-	# x = -(b1 - b2)/(a1 - a2) 를 이용하여 x를 구하고 직선 1에 x값을 대입하여 y를 구함.
-	else:
-		result.x = -(constant1 - constant2)/(increase1- increase2)
-		result.y = increase1 * result.x + constant1
-	return result
-  
+    # 직선 1이 y축에 평행한 경우
+    if(parallel1):
+        sameValue1 = x1.x
+    else:
+        increase1 = (x2.y - x1.y)/(x2.x - x1.x)  # 직선 1의 기울기
+        constant1 = x1.y - increase1 * x1.x  # 직선 1의 y절편
+
+    # 직선 2가 y축에 평행한 경우
+    if(parallel2):
+        sameValue2 = x3.x
+    else:
+        increase2 = (x4.y - x3.y)/(x4.x - x3.x)  # 직선 2의 기울기
+        constant2 = x3.y - increase2 * x3.x  # 직선 2의 y절편
+
+    # 두 직선이 평행한 경우 교점 없음. return None
+    if(parallel1 and parallel2):
+        return None
+
+    # 직선 1이 y축에 평행한 경우 직선 1의 직선 위에 교점의 x값이 존재.
+    # 직선 2의 y = ax + b 함수에 x값을 넣어 y값을 구함.
+    if(parallel1):
+        result = Point(sameValue1, increase2 * sameValue1 + constant2)
+    # 직선 2가 y축에 평행한 경우 직선 2의 직선 위에 교점의 x값이 존재.
+    # 직선 1의 y = ax + b 함수에 x값을 넣어 y값을 구함.
+    elif(parallel2):
+        result = Point(sameValue2, increase1 * sameValue2 + constant1)
+    # y축에 평행한 직선이 없는 경우
+    # x = -(b1 - b2)/(a1 - a2) 를 이용하여 x를 구하고 직선 1에 x값을 대입하여 y를 구함.
+    else:
+        result.x = -(constant1 - constant2)/(increase1 - increase2)
+        result.y = increase1 * result.x + constant1
+    return result
+
+
 print("[*] Input Point(x, y) : ")
 a = input()
 a = Point(a[0], a[-1])
--- Self-Driving/test.py
+++ Self-Driving/test.py
@@ -61,12 +61,54 @@ def get_fitline(img, f_lines):  # 대표선 구하기
         rows, cols = img.shape[:2]
         output = cv2.fitLine(lines, cv2.DIST_L2, 0, 0.01, 0.01)
         vx, vy, x, y = output[0], output[1], output[2], output[3]
-        print(vx, vy, x, y)
         if not np.isnan(vx):
             x1, y1 = int(((img.shape[0]-1)-y)/vy*vx + x), img.shape[0]-1
             x2, y2 = int(((img.shape[0]/2+50)-y)/vy*vx + x), int(img.shape[0]/2+50)
             result = [x1, y1, x2, y2]
+            print(x1, y1, x2, y2)
             return result
+
+
+def getIntersection(x1, x2, x3, x4):
+    # 두 점을 지나는 직선 끼리의 교점을 찾는 함수.
+    # 평행 여부 확인
+    parallel1 = (x1.x == x2.x)
+    parallel2 = (x3.x == x4.x)
+    # result init
+    result = cv2.Point()
+
+    # 직선 1이 y축에 평행한 경우
+    if(parallel1):
+        sameValue1 = x1.x
+    else:
+        increase1 = (x2.y - x1.y)/(x2.x - x1.x)  # 직선 1의 기울기
+        constant1 = x1.y - increase1 * x1.x  # 직선 1의 y절편
+
+    # 직선 2가 y축에 평행한 경우
+    if(parallel2):
+        sameValue2 = x3.x
+    else:
+        increase2 = (x4.y - x3.y)/(x4.x - x3.x)  # 직선 2의 기울기
+        constant2 = x3.y - increase2 * x3.x  # 직선 2의 y절편
+
+    # 두 직선이 평행한 경우 교점 없음. return None
+    if(parallel1 and parallel2):
+        return None
+
+    # 직선 1이 y축에 평행한 경우 직선 1의 직선 위에 교점의 x값이 존재.
+    # 직선 2의 y = ax + b 함수에 x값을 넣어 y값을 구함.
+    if(parallel1):
+        result = cv2.Point(sameValue1, increase2 * sameValue1 + constant2)
+    # 직선 2가 y축에 평행한 경우 직선 2의 직선 위에 교점의 x값이 존재.
+    # 직선 1의 y = ax + b 함수에 x값을 넣어 y값을 구함.
+    elif(parallel2):
+        result = cv2.Point(sameValue2, increase1 * sameValue2 + constant1)
+    # y축에 평행한 직선이 없는 경우
+    # x = -(b1 - b2)/(a1 - a2) 를 이용하여 x를 구하고 직선 1에 x값을 대입하여 y를 구함.
+    else:
+        result.x = -(constant1 - constant2)/(increase1 - increase2)
+        result.y = increase1 * result.x + constant1
+    return result
 
 
 # image = cv2.imread('slope_test.jpg')  # 이미지 읽기
@@ -114,6 +156,11 @@ while(cap.isOpened()):
                     draw_fit_line(temp, left_fit_line)
                 if(right_fit_line is not None):
                     draw_fit_line(temp, right_fit_line)
+                point1 = cv2.Point(left_fit_line[0], left_fit_line[1])
+                point2 = cv2.Point(left_fit_line[2], left_fit_line[3])
+                point3 = cv2.Point(right_fit_line[0], right_fit_line[1])
+                point4 = cv2.Point(right_fit_line[2], right_fit_line[3])
+                interPoint = getIntersection(point1, point2, point3, point4)
             except Exception as e:
                 print(e)
 
